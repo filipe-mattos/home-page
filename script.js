83acfe30b251a4ae99508e413745e12fa9d3f4dc
Filipe <filipe14mattos@gmail.com>
2020-09-09 21:43:24 -0300

    Subindo arquivos

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName("hr")[0].style.backgroundColor = "#FFBF00";
+document.getElementsByTagName("hr")[1].style.backgroundColor = "#FF9A1E";
+document.getElementsByTagName("hr")[2].style.backgroundColor = "#F15E1F";
+document.getElementsByTagName("hr")[3].style.backgroundColor = "#E7381C";
